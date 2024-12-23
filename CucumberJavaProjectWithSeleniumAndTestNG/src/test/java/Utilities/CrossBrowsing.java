49930b2c72a13f71d9c0ff1771093c430e52e452
pfeniza <pfeniza@ENIC-LDUERO.eurogroup.local>
2024-12-23 09:44:51 +0800

    ChangeUtilities

--- CucumberJavaProjectWithSeleniumAndTestNG/src/test/java/Utilities/CrossBrowsing.java
+++ CucumberJavaProjectWithSeleniumAndTestNG/src/test/java/Utilities/CrossBrowsing.java
@@ -2,6 +2,7 @@ package Utilities;
 
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 
@@ -9,13 +10,19 @@ public class CrossBrowsing {
 	
 
 	public static WebDriver open(String browser) {
-		  if (browser.equalsIgnoreCase("google")) {
-			   return new ChromeDriver ();
+		 if (browser.equalsIgnoreCase("google")) {
+	            // Set up ChromeOptions for headless mode
+	            ChromeOptions options = new ChromeOptions();
+	            options.addArguments("--headless"); // Run in headless mode
+	            options.addArguments("--disable-gpu"); // Disable GPU hardware acceleration
+	            options.addArguments("--no-sandbox"); // Disable sandboxing, necessary on some systems
+	            options.addArguments("--remote-debugging-port=9222"); // Allow remote debugging
 
-		   }else {
-			    return new FirefoxDriver();
-			    
-		   }
-
-}
+	            // Return the ChromeDriver with the specified options
+	            return new ChromeDriver(options);
+	        } else {
+	            // Return FirefoxDriver if the browser is not Google Chrome
+	            return new FirefoxDriver();
+	        }
+	    }
 }
